762060804aa94b4529bebc64ba5f795e9d2cd83d
ashadria <ashadria@gmail.com>
2021-01-21 20:30:25 -0500

    adding demo video, updated app logic

--- Develop/app.js
+++ Develop/app.js
@@ -112,3 +112,68 @@ const employeeQuestions = [
     }
 ]
 
+function buildTeamList() {
+    inquire.prompt(employeeQuestions).then(employeeInfo => {
+        if (employeeInfo.role == "engineer") {
+            var newMember = new Engineer(employeeInfo.name, teamList.length + 1, employeeInfo.email, employeeInfo.github);
+        } else {
+            var newMember = new Intern(employeeInfo.name, teamList.length + 1, employeeInfo.email, employeeInfo.school);
+        }
+        teamList.push(newMember);
+        if (employeeInfo.addAnother === "Yes") {
+            console.log(" ");
+            buildTeamList();
+        } else {
+            buildHtmlPage();
+        }
+    })
+}
+
+function buildHtmlPage() {
+    let newFile = fs.readFileSync("./templates/main.html")
+    fs.writeFileSync("./output/teamPage.html", newFile, function (err) {
+        if (err) throw err;
+    })
+
+    console.log("Template generated!");
+
+    for (member of teamList) {
+        if (member.getRole() == "Manager") {
+            buildHtmlCard("manager", member.getName(), member.getId(), member.getEmail(), "Office: " + member.getOfficeNumber());
+        } else if (member.getRole() == "Engineer") {
+            buildHtmlCard("engineer", member.getName(), member.getId(), member.getEmail(), "Github: " + member.getGithub());
+        } else if (member.getRole() == "Intern") {
+            buildHtmlCard("intern", member.getName(), member.getId(), member.getEmail(), "School: " + member.getSchool());
+        }
+    }
+    fs.appendFileSync("./output/teamPage.html", "</div></main></body></html>", function (err) {
+        if (err) throw err;
+    });
+    console.log("Operation completed.")
+
+}
+
+function buildHtmlCard(memberType, name, id, email, propertyValue) {
+    let data = fs.readFileSync(`./templates/${memberType}.html`, 'utf8')
+    data = data.replace("nameHere", name);
+    data = data.replace("idHere", `ID: ${id}`);
+    data = data.replace("emailHere", `Email: <a href="mailto:${email}">${email}</a>`);
+    data = data.replace("propertyHere", propertyValue);
+    fs.appendFileSync("./output/teamPage.html", data, err => { if (err) throw err; })
+    console.log("Card appended");
+}
+
+function init() {
+    inquire.prompt(managerQuestions).then(managerInfo => {
+        let teamManager = new Manager(managerInfo.name, 1, managerInfo.email, managerInfo.officeNum);
+        teamList.push(teamManager);
+        console.log(" ");
+        if (managerInfo.hasTeam === "Yes") {
+            buildTeamList();    
+        } else {
+            buildHtmlPage();
+        }
+    })
+}
+
+init();
